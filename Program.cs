5052d3d79e731d80acceaad4739e136ead9a7e32
rummolprod999 <rummolprod999@gmail.com>
2019-06-20 09:57:53 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace erewr
+{
+    class Program
+    {
+        private static string result;
+
+        static void Main()
+        {
+            SaySomething().Wait();
+            SaySomething1().Wait();
+        }
+
+        static async Task SaySomething()
+        {
+            Console.WriteLine("Before");
+            await Task.Factory.StartNew(async () => { await Task.Delay(5000); });
+            Console.WriteLine("After");
+        }
+
+        static async Task SaySomething1()
+        {
+            Console.WriteLine("Before");
+            await Task.Run(async () => { await Task.Delay(5000); Console.WriteLine("After 5 seconds sleep");  await Task.Delay(5000); });
+            Console.WriteLine("After");
+        }
+    }
+}
