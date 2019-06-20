a802a2b96c9193c40c479ee6f26fb2f9a8f49943
rummolprod999 <rummolprod999@gmail.com>
2019-06-20 10:05:04 +0300

    fix

--- Program.cs
+++ Program.cs
@@ -18,14 +18,14 @@ namespace erewr
         static async Task SaySomething()
         {
             Console.WriteLine("SaySomething Before");
-            await Task.Factory.StartNew(async () => { await Task.Delay(5000); });
+            await Task.Factory.StartNew(async () => { await Task.Delay(5000); Console.WriteLine("SaySomething After 5 seconds sleep");});
             Console.WriteLine("SaySomething After");
         }
 
         static async Task SaySomething1()
         {
             Console.WriteLine("SaySomething1 Before");
-            await Task.Run(async () => { await Task.Delay(5000); Console.WriteLine("After 5 seconds sleep");  await Task.Delay(5000); });
+            await Task.Run(async () => { await Task.Delay(5000); Console.WriteLine("SaySomething1 After 5 seconds sleep");  await Task.Delay(5000); });
             Console.WriteLine("SaySomething1 After");
         }
 
